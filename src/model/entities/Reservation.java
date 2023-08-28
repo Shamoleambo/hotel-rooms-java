08145eea4f0aa65266d897629386e589cdff739c
Tiago Gomes <tidgomes@gmail.com>
2023-08-28 14:58:16 -0300

    add check for date in Reservation constructor

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -12,7 +12,14 @@ public class Reservation {
 	private Date checkOut;
 	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 	
-	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) {
+	public Reservation(Integer roomNumber, Date checkIn, Date checkOut) throws DomainException {
+		Date now = new Date();
+		if(checkIn.before(now) || checkOut.before(now)) {
+			throw new DomainException("Reservation dates for update must be future dates");
+		}
+		if(!checkOut.after(checkIn)) {
+			throw new DomainException("Check-out date must be after check-in date");
+		}
 		this.roomNumber = roomNumber;
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
